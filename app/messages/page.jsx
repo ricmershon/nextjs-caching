ece0a12303d56a320257846c83ee1291aebc1629
Ric Mershon <rmersh@ricmershon.com>
2024-10-25 16:34:05 -0400

    Additional data caching methods

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,14 +1,31 @@
+// 'unstable_noStore' is preferred over
+// export const dynamic = 'force-dynamic' because it is more granular (scoped to a
+// a function and not a file)
+// import { unstable_noStore } from 'next/cache';
+
 import Messages from '@/components/messages';
 
+// 'revalidate' is a Next.js reserved word. Same as setting on the fetch function
+// export const revalidate = 5;
+
+// 'dynamic' is a Next.js reserved word. Takes different values. 'force-dynamic'
+// is the same as cache: 'no-store'.
+// export const dynamic = 'force-dynamic';
+
+// 'fetchCache = 'force-no-store' is a Next.js reserved word and takes different values.
+// 'force-no-store' is the same as cache: 'no-store'.
+// export const fetchCache = 'force-no-store'
+
 export default async function MessagesPage() {
+    // unstable_noStore();
     // Next.js overrides the default fetch function and includes a 'cache' object
     // in the options object.
     const response = await fetch('http://localhost:8080/messages', {
         // cache: 'force-cache'    // Ensures response will be cached
         // cache: 'no-store'       // Does not cache response and always send new request
-        next: {
-            revalidate: 5,      // Number of seconds to reuse cache data
-        }
+        // next: {
+        //     revalidate: 5,      // Number of seconds to reuse cache data
+        // }
     });
     
     const messages = await response.json();
