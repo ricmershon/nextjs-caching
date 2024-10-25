41d0865b51ecd0ac762ac67d9b3b027eb2762635
Ric Mershon <rmersh@ricmershon.com>
2024-10-25 16:10:27 -0400

    Control data caching with Next.js fetch options object.

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,7 +1,15 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
-    const response = await fetch('http://localhost:8080/messages');
+    // Next.js overrides the default fetch function and includes a 'cache' object
+    // in the options object.
+    const response = await fetch('http://localhost:8080/messages', {
+        // cache: 'force-cache'    // Ensures response will be cached
+        // cache: 'no-store'       // Does not cache response and always send new request
+        next: {
+            revalidate: 5,      // Number of seconds to reuse cache data
+        }
+    });
     
     const messages = await response.json();
 
